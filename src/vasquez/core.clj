61e74222f507c4627da23137fef31fc76ba97bd3
Owain Lewis <owainlewis@Owains-MacBook-Pro.local>
2012-02-29 13:59:54 +0000

    tidying last commit

--- src/vasquez/core.clj
+++ src/vasquez/core.clj
@@ -14,7 +14,7 @@
   ([node attr]
     (.attr node attr))
   ([node attr attr-val]
-	(.attr node attr attr-val)))
+    (.attr node attr attr-val)))
  
 (defn get-links [doc]
   "Extract out all the links from a web page"
@@ -24,13 +24,13 @@
   (.select doc "img"))
 
 (defn get-page-hrefs [url]
-  "Collects all hrefs from a web page"
+  "Collect all hrefs from a web page"
   (map #(get-attr % "href") 
     (get-links 
       (get-url url))))
 
 (defn get-img-src-values [url]
-  "Collects all hrefs from a web page"
+  "Collect all image src values from a web page"
   (map #(get-attr % "src") 
     (get-images
       (get-url url))))
