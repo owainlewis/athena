0b07a730b0563f6a3dba68fba77bb681549bb184
Owain Lewis <owain@owainlewis.com>
2012-03-08 22:32:21 +0000

    minor changes

--- src/scout/core.clj
+++ src/scout/core.clj
@@ -4,18 +4,13 @@
             [org.jsoup.select Elements]
             [org.jsoup.nodes Element Document]))
 
-;; TODO
-;; Currently links are getting parsed multiple times. Partition links before parsing
-;; Handle exceptions better
+(defprotocol WebParser
+  "Protocol for reading and writing web pages"
+  (reader [f])
+  (writer [f obj]))
 
-;; Map reduce jobs on text. Count words on page, word frequency on a web page etc
-;; Clean up the core file so that it's actually useful. More elegant code
-;; Nice API for getting page elements
-;; Rewrite program with tests
-
-(defonce results (atom []))
-
-;;; STATUS CHECKING
+(defn response [url]
+  ((juxt :status :body) (client/get url)))
 
 (defn check-status
   "Returns the HTTP status code of a url"
@@ -29,20 +24,14 @@
 (defn not-found? [url]
   (= 404 (check-status url)))
 
-(defn uri?
-  "Return true if a valid uri is given"
-  ([uri]
-   (not 
-     (nil? 
-       (re-find #"^(http|https|file)://.*" uri)))))
+(defn uri? [uri]
+  (not (nil? (re-find #"^(http|https|file)://.*" uri))))
 
 (defn parse-full-url 
   [host, url]
   (cond (uri? url) url
         (.startsWith url "/") (str host url)
         :else host))
-
-;;; Parser
 
 (defn parse [html]
   (Jsoup/parse html))
@@ -65,25 +54,13 @@
   ([node attr attr-val]
     (.attr node attr attr-val)))
 
-(defmacro fetch [doc el & forms]
-  `(if (> 0 (count ~forms))
-    (map (fn [x] 
-      (get-attr x (first ~@forms))
-      (.select ~doc ~el)))))
+(defmacro fetch [doc el]
+  `(.select ~doc ~el))
 	      
 (defn get-links
   "Extract out all the links from a web page"
   [doc]
   (fetch doc "a"))
-
-(defn get-images [doc]
-  (.select doc "img"))
-
-(defn get-head [doc]
-  (.select doc "head"))
-
-(defn get-meta [doc]
-  (.select doc "meta"))
 
 (defmacro parse-attr [attr url]
   `(map #(get-attr % ~attr)
@@ -94,22 +71,13 @@
   [url]
   (distinct (parse-attr "href" url)))
 
-(defn get-img-src-values
-  "Collect all image src values from a web page"
-  [url]
-  (map 
-    #(get-attr % "src") 
-      (get-images
-        (get-url url))))
-
 (defn links->status
   "Maps every link on a page with a status code i.e [http://www.google.com 200]"
   [url]
   (let [links (get-page-hrefs url)] 
     (map 
-      #(vector 
-         (parse-full-url url %) 
-         (check-status (parse-full-url url %))) links)))
+      #(vector (parse-full-url url %) 
+               (check-status (parse-full-url url %))) links)))
 
 (defn broken-links
   "Extracts broken links from a crawl map"
@@ -117,11 +85,10 @@
   (println "Crawling for broken links...\n")
   (map first
     (filter 
-      (fn [x]
-        (not (= 200 (second x)))) res)))
+      (fn [x] (not (= 200 (second x)))) res)))
 
 (defn find-broken-links [uri]
   (let [results (broken-links (links->status uri))]
     (if (empty? results)
-        (println "No broken links")
-        results)))
+      (println "No broken links")
+      results)))
--- src/scout/frequency.clj
+++ src/scout/frequency.clj
@@ -1,11 +1,17 @@
 (ns scout.frequency
-  (:use clojure.java.io :as io))
+  (:use [clojure.java.io :as io]))
 
-(defn word-freq
-  "Create a word frequency map"
-  [f]
-  (->> f
-      io/read-lines
-      (mapcat (fn [l] (map #(.toLowerCase %) (re-seq #"\w+" l))))
-      (reduce #(assoc %1 %2 (inc (%1 %2 0))) {})
-      (sort-by (comp - val))))
+;; Tools for analysing word frequencies in text
+
+(defn parse-line
+  ""
+  [line]
+  (let [tokens (clojure.string/split (.toLowerCase line) #"\s+")]
+    (apply concat
+      (reduce (fn [a b]
+        (update-in a [b] (fnil inc 0))) {} tokens))))
+		   
+(defn combine [result]
+  (apply concat result))
+
+
