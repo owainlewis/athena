ec0ba457b19deddecc98928042bb9b7cf82b9f87
owain lewis <owain@owainlewis.com>
2013-12-02 16:23:07 +0000

    update project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject athena "1.0.1-SNAPSHOT"
+(defproject athena "1.0.2"
   :description "A web crawler written in Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.jsoup/jsoup "1.7.3"]]
