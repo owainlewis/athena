bdbb3afcfa8996061a96ce14e1299e9f84be319a
Owain Lewis <owain@owainlewis.com>
2014-11-30 20:13:11 +0000

    Update project.clj

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "A web crawler written in Clojure"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.16"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.jsoup/jsoup "1.7.3"]]
   :main athena.core)
