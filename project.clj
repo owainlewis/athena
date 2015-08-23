1bd1ee2f8da9cda9498dfee1521a4acaee2c04ad
Owain Lewis <owain@owainlewis.com>
2015-08-23 20:23:27 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.16"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.jsoup/jsoup "1.7.3"]]
+                 [org.jsoup/jsoup "1.8.2"]]
   :main athena.core)
